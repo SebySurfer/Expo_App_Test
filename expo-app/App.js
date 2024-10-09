100c1a0f41eae9788dc7d29dec587b931bd143f8
SebySurfer <sebysurfer@gmail.com>
2024-10-08 21:32:42 -0600

    Basic component

--- expo-app/App.js
+++ expo-app/App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import MyHeader from './components/MyHeader';
+
 export default function App() {
   return (
     <View style={styles.container}>
+      <MyHeader/>
       <Text>Open up App.js to start working on your app!</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
 }
+
+
+
 
 const styles = StyleSheet.create({
   container: {
